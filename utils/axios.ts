56e8d705da3361baabd113124d77e52434b07375
ijusthahaha <ijusthahaha@outlook.com>
2024-04-01 16:34:45 +0800

    impl dynamic baseurl

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,11 +1,23 @@
 import axios from "axios"
 import NProgress from "nprogress";
 
-const BASE_URL = "//localhost:8080"
+const BASE_URL = ""
+const BASE_PORT = "8080"
 
-axios.defaults.withCredentials = true
+const http = axios.create({
+    baseURL: BASE_URL, timeout: 10000
+});
 
-axios.interceptors.request.use(config => {
+http.defaults.withCredentials = true
+
+http.interceptors.request.use(config => {
+        if (config.baseURL === "") {
+            let protocol = window.location.protocol
+            let hostname = window.location.hostname
+            config.baseURL = protocol + "//" + hostname + ":" + BASE_PORT;
+            http.defaults.baseURL = protocol + "//" + hostname + ":" + BASE_PORT;
+        }
+
         NProgress.inc();
         return config;
     }, error => {
@@ -13,7 +25,8 @@ axios.interceptors.request.use(config => {
         return Promise.reject(error)
     }
 )
-axios.interceptors.response.use(res => {
+
+http.interceptors.response.use(res => {
         NProgress.done();
         return res;
     }, error => {
@@ -21,9 +34,5 @@ axios.interceptors.response.use(res => {
         return Promise.reject(error)
     }
 )
-
-const http = axios.create({
-    baseURL: BASE_URL, timeout: 10000
-});
 
 export default http
